46315c4d5c0678f20abc9ff39d623795061f24c8
sunny-sarath-2 <sunnsarath2@gmail.com>
2020-08-30 07:57:07 +0530

    chanes: - added env process

--- index.js
+++ index.js
@@ -72,6 +72,6 @@ app.use((error, req, res, next) => {
 });
 
 //server
-server.listen(config.httpPort, () => {
-  console.log(config.httpPort);
+server.listen(process.env.PORT, () => {
+  console.log(process.env.PORT);
 });
