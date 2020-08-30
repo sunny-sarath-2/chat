b3b4182045ccb0109767e09c1b87955cfb3a24d9
sunny-sarath-2 <sunnsarath2@gmail.com>
2020-08-30 08:12:37 +0530

    changes: - production config updated

--- services/config/env/production.js
+++ services/config/env/production.js
@@ -1,11 +1,9 @@
-const secrect = require("../secrect.json");
-
 module.exports = {
   errors: {
     showDebug: false,
   },
   database: {
-    server: secrect.database,
+    server: ENV["database"],
     database: "chat",
   },
 
